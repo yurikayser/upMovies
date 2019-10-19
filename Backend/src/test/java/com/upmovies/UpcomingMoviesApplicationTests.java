c38eb7fee8c6af58e45400afdbba12b2b2963a65
yurikayser <ykr337@gmail.com>
2019-10-19 16:55:34 -0300

    wip test

--- Backend/src/test/java/com/upmovies/UpcomingMoviesApplicationTests.java
+++ Backend/src/test/java/com/upmovies/UpcomingMoviesApplicationTests.java
@@ -1,12 +1,7 @@
 package com.upmovies;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class UpcomingMoviesApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
