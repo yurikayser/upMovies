5f40b2cc011d9eadb7b97543d5032cb08b09a752
yurikayser <ykr337@gmail.com>
2019-10-19 17:39:20 -0300

    wip rest

--- Backend/src/main/java/com/upmovies/controllers/UpcomingMoviesController.java
+++ Backend/src/main/java/com/upmovies/controllers/UpcomingMoviesController.java
@@ -5,15 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("upcoming-movies")
 public class UpcomingMoviesController {
 
-	@GetMapping(value = "/get-movies", produces = "apllication/json")
+	@RequestMapping(value = "/movies")
 	public String getUpcomingMovies() {
 		return "just testing";
 	}
 
-	@GetMapping(value = "/get-movies-category/{category}", produces = "apllication/json")
+	@GetMapping(value = "/get-movies-category/{category}")
 	public String getUpcomingMoviesByCategory(Long category) {
 		return "you sent: " + category;
 	}
