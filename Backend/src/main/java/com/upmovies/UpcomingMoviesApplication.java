2b2d573308069377c644f332c231f31a856e71d8
yurikayser <ykr337@gmail.com>
2019-10-19 11:37:17 -0300

    Inicio springboot

---
+++ Backend/src/main/java/com/upmovies/UpcomingMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.upmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcomingMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcomingMoviesApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/upmovies/UpcomingMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcomingMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
