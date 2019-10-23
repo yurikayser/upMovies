5bb3567ec8247f0825103af4eac05c2567485a9c
yurikayser <ykr337@gmail.com>
2019-10-23 03:57:32 -0300

    alteracao url

--- Backend/src/main/Frontend/client/src/requests/requests.js
+++ Backend/src/main/Frontend/client/src/requests/requests.js
@@ -1,13 +1,13 @@
 export function requestUpcomingMovies(page) {
   return fetch(
-    "http://ec2-18-222-203-163.us-east-2.compute.amazonaws.com:8080/movies?page=" +
+    "http://ec2-18-219-8-21.us-east-2.compute.amazonaws.com:8080/movies?page=" +
       page
   ).then(response => response.json());
 }
 
 export function requestSearchMovies(page, name) {
   return fetch(
-    "http://ec2-18-222-203-163.us-east-2.compute.amazonaws.com:8080/movies/search?page=" +
+    "http://ec2-18-219-8-21.us-east-2.compute.amazonaws.com:8080/movies/search?page=" +
       page +
       "&movieName=" +
       name
@@ -16,7 +16,7 @@ export function requestSearchMovies(page, name) {
 
 export function requestMovieDetails(id) {
   return fetch(
-    "http://ec2-18-222-203-163.us-east-2.compute.amazonaws.com:8080/movies/detail?movieId=" +
+    "http://ec2-18-219-8-21.us-east-2.compute.amazonaws.com:8080/movies/detail?movieId=" +
       id
   ).then(response => response.json());
 }
