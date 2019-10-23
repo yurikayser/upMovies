e5be6bc5623f6e7a61501d178bf80709bc8e0ad9
yurikayser <ykr337@gmail.com>
2019-10-23 02:58:36 -0300

    ajuste url

--- Backend/src/main/Frontend/client/src/requests/requests.js
+++ Backend/src/main/Frontend/client/src/requests/requests.js
@@ -1,17 +1,22 @@
 export function requestUpcomingMovies(page) {
-  return fetch("http://localhost:8080/movies?page=" + page).then(response =>
-    response.json()
-  );
+  return fetch(
+    "http://ec2-18-222-203-163.us-east-2.compute.amazonaws.com:8080/movies?page=" +
+      page
+  ).then(response => response.json());
 }
 
 export function requestSearchMovies(page, name) {
   return fetch(
-    "http://localhost:8080/movies/search?page=" + page + "&movieName=" + name
+    "http://ec2-18-222-203-163.us-east-2.compute.amazonaws.com:8080/movies/search?page=" +
+      page +
+      "&movieName=" +
+      name
   ).then(response => response.json());
 }
 
 export function requestMovieDetails(id) {
-  return fetch("http://localhost:8080/movies/detail?movieId=" + id).then(
-    response => response.json()
-  );
+  return fetch(
+    "http://ec2-18-222-203-163.us-east-2.compute.amazonaws.com:8080/movies/detail?movieId=" +
+      id
+  ).then(response => response.json());
 }
